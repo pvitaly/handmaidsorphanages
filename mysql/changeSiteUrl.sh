c40fe1e02ba2d30fd7cd3b17d0bf3f0550745819
Matt Juntunen <matt.juntunen@hotmail.com>
2014-01-14 23:03:23 -0500

    adding shell script to change the site url

---
+++ mysql/changeSiteUrl.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+# changeSiteUrl.sh
+# A script to change the base url of the WordPress site
+
+DB=wp_handmaids
+USER=handmaids
+PSWD=handmaids
+
+if [ $# -ne 1 ]; then
+	echo "USAGE: $0 [base url without trailing slash]" && exit 0
+fi
+
+echo "changing WordPress siteurl to $1"
+
+oldhost=$(mysql -h localhost -u $USER -p${PSWD} $DB -e "select option_value from wp_options where option_name = 'siteurl'" )
+oldhost=$(echo $oldhost | perl -ne 'if ($_ =~ m/(http:\/\/\S+)/) { print $1; }')
+if [ -e $oldhost ]; then
+	echo "Could not find current host name. Operation aborted." && exit 1
+fi
+
+echo "previous siteurl: $oldhost"
+
+script="update wp_options set option_value = '$1' where option_name = 'siteurl'; "
+script=${script}"update wp_posts set guid = replace( guid, '$oldhost', '$1' ) where instr( guid, '$oldhost') = 1;"
+
+mysql -h localhost -u $USER -p${PSWD} $DB -e "$script"
+
+if [ $? -eq 0 ]; then
+	echo "operation succeeded. New siteurl is $1"
+	exit 0
+else
+	echo "operation failed!"
+	exit 1
+fi
