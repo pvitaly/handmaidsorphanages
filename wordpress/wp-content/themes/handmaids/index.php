43661624de3717eae0e60610e1d29c4075e758a7
Matt Juntunen <matt.juntunen@hotmail.com>
2014-01-14 23:30:50 -0500

    creating theme folder and essential files; adding new mysql dump

---
+++ wordpress/wp-content/themes/handmaids/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This is the main page of the Handmaids of the Blessed Trinity Orphanages site.
+ */
+?>
+
+<html>
+	<head>
+		<title>Handmaids of the Blessed Trinity Orphanages</title>
+	</head>
+	<body>
+		<h3>Handmaids of the Blessed Trinity Orphanages</h3>
+	</body>
+</html>
