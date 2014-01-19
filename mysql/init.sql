ea25ba9fd0e57bd1fd393fc05fe0eebdd493fd57
Michael Taulbee <mtaulbee@miketaulbee.com>
2014-01-19 11:43:01 -0500

    added use wp_handmaids;, it makes sure the sql code executes on the correct database.
    Signed-off-by: Michael Taulbee <mtaulbee@miketaulbee.com>

--- mysql/init.sql
+++ mysql/init.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `wp_commentmeta`
 --
-
+use wp_handmaids;
 DROP TABLE IF EXISTS `wp_commentmeta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
