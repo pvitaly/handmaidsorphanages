f747ff3ba7262849b5a66233b995079c6e478427
Vitaly Peker <pekerv@gmail.com>
2014-01-26 15:45:54 -0500

    Vitaly: Added carousel for images on the front page. Right now - only two images are displayed because they have the same width and height. Going forward, images in the carousel should be consistent dimensions.

--- wordpress/wp-content/themes/handmaids/front-page.php
+++ wordpress/wp-content/themes/handmaids/front-page.php
@@ -8,12 +8,35 @@ get_header();
 // photo carousel
 $img_list = Image::get_carousel_images();
 
-echo '<div id="photo-carousel">';
+echo '<div id="carousel-div" class="row">';
+echo '<div id="carousel-generic" class="carousel slide" data-ride="carousel">';
+// indicators 
+echo ' <ol class="carousel-indicators">';
+echo '<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>';
+echo '<li data-target="#carousel-example-generic" data-slide-to="1"></li>';
+echo '</ol>';
+echo '<div class="carousel-inner">';
+echo '<div class="item active">';
+$img = $img_list[1];
+echo "<img class='carousel-image img-rounded' src='$img->guid'/>";
+echo '</div>';
+echo '<div class="item">';
+$img2 = $img_list[2];
+echo "<img class='carousel-image img-rounded' src='$img2->guid'/>";
+echo '</div>';
+echo '</div>';
 
-//display a random picture for now
-$idx = rand(0, count($img_list) - 1);
-$img = $img_list[$idx];
-echo "<img src='$img->guid'/>";
+// Controls
+echo '<a class="left carousel-control" href="#carousel-generic" data-slide="prev">';
+echo '<span class="glyphicon glyphicon-chevron-left"></span>';
+echo '</a>';
+echo '<a class="right carousel-control" href="#carousel-generic" data-slide="next">';
+echo '<span class="glyphicon glyphicon-chevron-right"></span>';
+echo '</a>';
+echo '</div>';
+echo '</div>';
+
+
 
 // foreach ($img_list as $img){
 	// echo "<img src='$img->guid'/>";
