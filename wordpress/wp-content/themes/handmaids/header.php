eb8ce5e326a99016f2e588bfa139b98a6b5446a0
Matt Juntunen <matt.juntunen@hotmail.com>
2014-03-12 21:40:58 -0400

    changing main title to be pulled from bloginfo instead of hardcoded

--- wordpress/wp-content/themes/handmaids/header.php
+++ wordpress/wp-content/themes/handmaids/header.php
@@ -54,7 +54,7 @@ function build_page_menu() {
 		<div id="header">
 			<div class="wrapper clearfix">
 				<img class="logo" src="<?php echo get_stylesheet_directory_uri() . "/img/logo.png"; ?>"/>
-				<div id="mainTitle">handmaids of the blessed trinity orphanages</div>
+				<div id="mainTitle"><?php echo strtolower(get_bloginfo('name')) ?></div>
 				<?php build_page_menu(); ?>
 			</div>
 		</div>
