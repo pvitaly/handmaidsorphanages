552f317dfe7d998aabb595afb8bb3feca2603ab3
pvitaly <pekerv@gmail.com>
2014-03-23 16:05:31 -0400

    Generalized the carousel to be a string input instead of checkbox. Added carousel shortcode with name attribute.

--- wordpress/wp-content/themes/handmaids/functions.php
+++ wordpress/wp-content/themes/handmaids/functions.php
@@ -1,100 +1,124 @@
-<?php 
+<?php
+
 /*
  * This file containing the initialization and library functions for the handmaids theme.
  */
- 
+
 //include some class files
 require 'includes/class-pods-helper.php';
 require 'includes/class-image.php';
 require 'includes/class-page.php';
 
 //load the admin page ui if needed
-if (is_admin()){
-	require 'includes/admin-menu.php';
+if (is_admin()) {
+    require 'includes/admin-menu.php';
 }
 
 /*
  * Set up css and script injection 
- */ 
-function enqueue_theme_styles() {
-	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
-	wp_enqueue_style('content', get_template_directory_uri() . '/css/content.css');
-	wp_enqueue_style('main-style', get_stylesheet_uri());
-}
-function enqueue_theme_scripts() {
-	$baseJsUri = get_template_directory_uri() . '/js/';
+ */
 
-	//move jquery to the footer
-	wp_deregister_script('jquery');
-	wp_register_script('jquery', '/wp-includes/js/jquery/jquery.js', false /* no dependencies*/, '1.10.2', true /* in footer*/);
-	wp_enqueue_script('jquery');
-	
-	wp_enqueue_script('bootstrap', $baseJsUri . 'bootstrap.js', array('jquery'), null, true);
-	wp_enqueue_script('main-script', $baseJsUri . 'handmaids.js', array('jquery', 'bootstrap'), null, true);
+function enqueue_theme_styles() {
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('content', get_template_directory_uri() . '/css/content.css');
+    wp_enqueue_style('main-style', get_stylesheet_uri());
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_theme_styles' );
-add_action( 'wp_enqueue_scripts', 'enqueue_theme_scripts' );
+
+function enqueue_theme_scripts() {
+    $baseJsUri = get_template_directory_uri() . '/js/';
+
+    //move jquery to the footer
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', '/wp-includes/js/jquery/jquery.js', false /* no dependencies */, '1.10.2', true /* in footer */);
+    wp_enqueue_script('jquery');
+
+    wp_enqueue_script('bootstrap', $baseJsUri . 'bootstrap.js', array('jquery'), null, true);
+    wp_enqueue_script('main-script', $baseJsUri . 'handmaids.js', array('jquery', 'bootstrap'), null, true);
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
+add_action('wp_enqueue_scripts', 'enqueue_theme_scripts');
 
 /**
-register shortcodes
-*/
-
+  register shortcodes
+ */
 //shortcode for creating a page title
-function pagetitle_handler($atts, $content = null){
-	return '<div class="page-title row">' . $content . '</div>';
+function pagetitle_handler($atts, $content = null) {
+    return '<div class="page-title row">' . $content . '</div>';
 }
+
 add_shortcode('pagetitle', 'pagetitle_handler');
 
 //shortcode for creating a section title
-function sectiontitle_handler($atts, $content = null){
-	return '<div class="section-title row">' . $content . '</div>';
+function sectiontitle_handler($atts, $content = null) {
+    return '<div class="section-title row">' . $content . '</div>';
 }
+
 add_shortcode('sectiontitle', 'sectiontitle_handler');
 
 
 //shortcode for google maps
 $map_count = 0;
-function googlemap_handler($atts){
-	global $map_count;
 
-	extract ( shortcode_atts ( array(
-		'lat' => 42.2733204,
-		'long' => -83.7376894,
-		'zoom' => 8,
-		'width' => -1,
-		'height' => -1
-	), $atts));
-	
-	$id = "googlemap$map_count";
-	
-	$result = '';
-	if ($map_count == 0) { //first map on the page
-		$result = '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>';
-	}
-	
-	$result .= "<div id='$id' class='googlemap' style='"; 
-				
-	if ($width > -1){
-			$result .= "width: ${width}px;";
-	} else {
-			$result .= "width: 100%;";
-	}
-	
-	if ($height > -1){
-		$result .= "height: ${height}px;";
-	} else {
-		$result .= "height: 100%;";
-	}
-	
-	
-	$result .= "'></div>";
-	$result .= "<script type='text/javascript'>function initialize_$id(){var opts = {center: new google.maps.LatLng($lat, $long), zoom: $zoom };";
-	$result .= "var map = new google.maps.Map(document.getElementById('$id'), opts);}";
-	$result .= "google.maps.event.addDomListener(window, 'load', initialize_$id);";
-	$result .= "</script>";
-	
-	$map_count++;
-	
-	return $result;
+function googlemap_handler($atts) {
+    global $map_count;
+
+    extract(shortcode_atts(array(
+        'lat' => 42.2733204,
+        'long' => -83.7376894,
+        'zoom' => 8,
+        'width' => -1,
+        'height' => -1
+                    ), $atts));
+
+    $id = "googlemap$map_count";
+
+    $result = '';
+    if ($map_count == 0) { //first map on the page
+        $result = '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>';
+    }
+
+    $result .= "<div id='$id' class='googlemap' style='";
+
+    if ($width > -1) {
+        $result .= "width: ${width}px;";
+    } else {
+        $result .= "width: 100%;";
+    }
+
+    if ($height > -1) {
+        $result .= "height: ${height}px;";
+    } else {
+        $result .= "height: 100%;";
+    }
+
+
+    $result .= "'></div>";
+    $result .= "<script type='text/javascript'>function initialize_$id(){var opts = {center: new google.maps.LatLng($lat, $long), zoom: $zoom };";
+    $result .= "var map = new google.maps.Map(document.getElementById('$id'), opts);}";
+    $result .= "google.maps.event.addDomListener(window, 'load', initialize_$id);";
+    $result .= "</script>";
+
+    $map_count++;
+
+    return $result;
 }
+
 add_shortcode('googlemap', 'googlemap_handler');
+
+function carousel($atts) {
+
+    extract(shortcode_atts(array(
+        'name' => 'school'
+                    ), $atts));
+
+    $img_list = Image::get_carousel_images($name);
+
+    $content_context = array(
+        'img_list' => $img_list
+    );
+
+    return Timber::compile('generic-carousel.twig', $content_context);
+}
+
+add_shortcode('carousel', 'carousel');
--- wordpress/wp-content/themes/handmaids/includes/class-image.php
+++ wordpress/wp-content/themes/handmaids/includes/class-image.php
@@ -1,66 +1,76 @@
 <?php
+
 /*
-A class for working with images.
-*/
+  A class for working with images.
+ */
+
 class Image {
-	
-	public static $PODS_NAME = 'media';
-	
-	/**
-	The id of the image
-	*/
-	public $ID;
-	
-	/**
-	The guid of the image
-	*/
-	public $guid;
-	
-	/**
-	The description of the image.
-	*/
-	public $post_content;
-	
-	/**
-	Boolean value indicating whether or not this image should be displayed in the main
-	page image carousel.
-	*/
-	public $show_in_carousel;
-	
-	
-	/**
-	Returns the url for the image, firing all appropriate triggers 
-	*/
-	public function get_url(){
-		return wp_get_attachment_url($ID);
-	}
-	
-	/**
-	Loads the given image by its id.
-	*/
-	public static function get_image_by_id($ID){
-		$params = array(
-				'where' => "t.ID = '$ID'"
-			);
-		return PodsHelper::find_one(__CLASS__, $params);	
-	}
-	
-	/**
-	Retrieves Images from the database
-	*/
-	public static function get_images(){
-			$params = array(
-					'orderby' => 'ID'
-					);
-				
-			return PodsHelper::find(__CLASS__, $params);
-	}
-	
-	public static function get_carousel_images(){
-			$params = array(
-					'where' => "show_in_carousel.meta_value = 1"
-				);
-			
-			return PodsHelper::find( __CLASS__, $params);
-	}
+
+    public static $PODS_NAME = 'media';
+
+    /**
+      The id of the image
+     */
+    public $ID;
+
+    /**
+      The guid of the image
+     */
+    public $guid;
+
+    /**
+      The description of the image.
+     */
+    public $post_content;
+
+    /**
+      Boolean value indicating whether or not this image should be displayed in the main
+      page image carousel.
+     */
+    public $show_in_carousel;
+
+    /**
+      Boolean value indicating whether or not this image should be displayed in the school
+      page image carousel.
+     */
+    public $show_in_school_carousel;
+
+    /**
+      Returns the url for the image, firing all appropriate triggers
+     */
+    public function get_url() {
+        return wp_get_attachment_url($ID);
+    }
+
+    /**
+      Loads the given image by its id.
+     */
+    public static function get_image_by_id($ID) {
+        $params = array(
+            'where' => "t.ID = '$ID'"
+        );
+        return PodsHelper::find_one(__CLASS__, $params);
+    }
+
+    /**
+      Retrieves Images from the database
+     */
+    public static function get_images() {
+        $params = array(
+            'orderby' => 'ID'
+        );
+
+        return PodsHelper::find(__CLASS__, $params);
+    }
+
+    public static function get_carousel_images($carousel_name) {
+        $where = "carousel.meta_value like '%" . $carousel_name . "%'";
+
+        $params = array(
+            'where' => $where
+        );
+
+        return PodsHelper::find(__CLASS__, $params);
+    }
+
 }
